073740f2fb9bcdbea6c724ce2e7c10e3f15927bc
sadat <sadat.himel@gmail.com>
2021-01-19 14:46:14 +0600

    dynamic index.php /Home page

--- index.php
+++ index.php
@@ -11,8 +11,21 @@
 
 <div class="contentsection contemplete clear">
 <div class="maincontent clear">
+
+    <!--Pagination -->
+        <?php
+            $per_page = 3;
+            if (isset($_GET["page"])){
+                $page = $_GET["page"];
+            } else {
+                $page = 1;
+            }
+            $start_form = ($page-1)*$per_page;
+        ?>
+    <!--Pagination -->
+
     <?php
-        $query = "SELECT * FROM tbl_post limit 3";
+        $query = "SELECT * FROM tbl_post limit $start_form,$per_page";
         $post = $db->select($query);
         if ($post) {
             while ($result = $post->fetch_assoc()) {
@@ -30,7 +43,22 @@
 		</div>
 	</div>
     <?php } ?> <!--end while loop -->
-    <?php }else{header("Location:404.php");} ?>
+
+            <!--Pagination -->
+                <?php
+                $query = "select * from tbl_post";
+                $result = $db->select($query);
+                $total_rows = mysqli_num_rows($result);
+                $total_pages = ceil($total_rows/$per_page);
+
+                echo "<span class='pagination'><a href='index.php?page=1'>".'First Page'."</a>";
+                for ($i=1; $i <= $total_pages; $i++){
+                   echo "<a href='index.php?page=".$i."'>".$i."</a>";
+                };
+                echo "<a href='index.php?page=$total_pages'>".'Last Page'."</a></span>"?>
+            <!--Pagination-->
+            
+    <?php } else { header("Location:404.php");} ?>
 </div>
 
 <?php include "inc/sidebar.php"; ?>
