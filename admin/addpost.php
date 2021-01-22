86c73ca6347d8d26ea3f2cf55ef0b3da43ff22da
sadat <sadat.himel@gmail.com>
2021-01-22 23:26:55 +0600

    add post option dynamic

--- admin/addpost.php
+++ admin/addpost.php
@@ -4,8 +4,46 @@
 
     <div class="box round first grid">
         <h2>Add New Post</h2>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = mysqli_real_escape_string($db->link, $_POST['title']);
+    $cat = mysqli_real_escape_string($db->link, $_POST['cat']);
+    $body = mysqli_real_escape_string($db->link, $_POST['body']);
+    $tag = mysqli_real_escape_string($db->link, $_POST['tag']);
+    $author = mysqli_real_escape_string($db->link, $_POST['author']);
+
+    $permited  = array('jpg','jpeg','png','gif');
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
+    $file_temp = $_FILES['image']['tmp_name'];
+
+    $div           = explode('.',$file_name);
+    $file_ext      = strtolower(end($div));
+    $uniqe_image   = substr(md5(time()), 0,10).'.'.$file_ext;
+    $uploded_image = "upload/".$uniqe_image;
+
+    if ($title == "" || $cat == "" || $body == "" || $tag == "" || $author == "" || $file_name == "") {
+        echo "<span class='error'>Field must not be empty !</span>";
+    } elseif ($file_size > 1048567) {
+        echo "<span class='error'>Image Size should be less then 1MB!</span>";
+    } elseif (in_array($file_ext, $permited) === false) {
+        echo "<span class='error'>You can upload only:-".implode(', ', $permited)."!</span>";
+    }else {
+        move_uploaded_file($file_temp, $uploded_image);
+        $query = "INSERT INTO tbl_post(cat, title, body, image, author, tags) 
+                VALUES('$cat', '$title', '$body', '$uploded_image', '$author', '$tags')";
+
+        $insert_rows = $db->insert($query);
+        if ($insert_rows) {
+            echo "<span class='success'>Post insert successfully!</span>";
+        }else {
+            echo "<span class='error'>Post not Inserted !</span>";
+        }
+    }
+}
+?>
         <div class="block">
-         <form action="" method="" enctype="multipart/form-data">
+         <form action="addpost.php" method="post" enctype="multipart/form-data">
             <table class="form">
 
                 <tr>
@@ -13,7 +51,7 @@
                         <label>Title</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Enter Post Title..." class="medium" />
+                        <input type="text" name="title" placeholder="Enter Post Title..." class="medium" />
                     </td>
                 </tr>
 
@@ -22,11 +60,16 @@
                         <label>Category</label>
                     </td>
                     <td>
-                        <select id="select" name="select">
+                        <select id="select" name="cat">
                             <option>Select Category</option>
-                            <option value="1">Category One</option>
-                            <option value="2">Category Two</option>
-                            <option value="3">Cateogry Three</option>
+                    <?php
+                        $query = "SELECT * FROM tbl_category";
+                        $category = $db->select($query);
+                        if ($category) {
+                            while ($result = $category->fetch_assoc()) {
+                    ?>
+                            <option value="<?php echo $result['id']; ?>"><?php echo $result['name']; ?></option>
+                            <?php } }?>
                         </select>
                     </td>
                 </tr>
@@ -36,7 +79,7 @@
                         <label>Upload Image</label>
                     </td>
                     <td>
-                        <input type="file" />
+                        <input type="file" name="image"/>
                     </td>
                 </tr>
                 <tr>
@@ -44,9 +87,28 @@
                         <label>Content</label>
                     </td>
                     <td>
-                        <textarea class="tinymce"></textarea>
+                        <textarea class="tinymce" name="body"></textarea>
                     </td>
                 </tr>
+                <tr>
+                    <td>
+                        <label>Tag</label>
+                    </td>
+                    <td>
+                        <input type="text" name="tag" placeholder="Enter tag..." class="medium" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>Author</label>
+                    </td>
+                    <td>
+                        <input type="text" name="author" placeholder="Enter author name..." class="medium" />
+                    </td>
+                </tr>
+
+
                 <tr>
                     <td></td>
                     <td>
