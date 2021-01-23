3303f18646d7275be2e9e35da113cb0f00369dba
sadat <sadat.himel@gmail.com>
2021-01-23 23:39:47 +0600

    slogan select

--- admin/titleslogan.php
+++ admin/titleslogan.php
@@ -1,38 +1,66 @@
 ﻿<?php include 'inc/header.php'; ?>
 <?php include 'inc/sidebar.php'; ?>
+<style>
+    .leftside{float: left;width: 70%;}
+    .rightside{float: left;width: 20%}
+    .rightside img{height: 160px; width: 170px;}
+</style>
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Update Site Title and Description</h2>
+        <?php
+            $query = "SELECT * FROM title_slogan WHERE id = '1'";
+            $blog_title = $db->select($query);
+            if ($blog_title) {
+            while ($result = $blog_title->fetch_assoc()) {
+        ?>
         <div class="block sloginblock">
-         <form>
-            <table class="form">
-                <tr>
-                    <td>
-                        <label>Website Title</label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Enter Website Title..."  name="title" class="medium" />
-                    </td>
-                </tr>
-                 <tr>
-                    <td>
-                        <label>Website Slogan</label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Enter Website Slogan..." name="slogan" class="medium" />
-                    </td>
-                </tr>
+            <div class="leftside">
+             <form>
+                <table class="form">
+                    <tr>
+                        <td>
+                            <label>Website Title</label>
+                        </td>
+                        <td>
+                            <input type="text" value="<?php echo $result['title']; ?>"  name="title" class="medium" />
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            <label>Website Slogan</label>
+                        </td>
+                        <td>
+                            <input type="text" value="<?php echo $result['slogan']; ?>" name="slogan" class="medium" />
+                        </td>
+                    </tr>
 
-                 <tr>
-                    <td>
-                    </td>
-                    <td>
-                        <input type="submit" name="submit" Value="Update" />
-                    </td>
-                </tr>
-            </table>
-            </form>
+                    <tr>
+                        <td>
+                            <label>Upload Image</label>
+                        </td>
+                        <td>
+                            <input type="file" name="logo"/>
+                        </td>
+                    </tr>
+
+                     <tr>
+                        <td>
+                        </td>
+                        <td>
+                            <input type="submit" name="submit" Value="Update" />
+                        </td>
+                    </tr>
+                </table>
+                </form>
+            </div>
+
+            <div class="rightside">
+                <img src="<?php echo $result['logo'];?>" alt="logo">
+                logo
+            </div>
         </div>
+        <?php } } ?>
     </div>
 </div>
 <?php include 'inc/footer.php'; ?>
