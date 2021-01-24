22295fdf94ef78dbc91f50aca78e534187373501
sadat <sadat.himel@gmail.com>
2021-01-24 12:16:37 +0600

    social media dynamic

--- admin/social.php
+++ admin/social.php
@@ -3,15 +3,54 @@
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Update Social Media</h2>
+        <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $fb = $fm->validation($_POST['fb']);
+                $tw = $fm->validation($_POST['tw']);
+                $ln = $fm->validation($_POST['ln']);
+                $gp = $fm->validation($_POST['gp']);
+
+                $fb = mysqli_real_escape_string($db->link, $fb);
+                $tw = mysqli_real_escape_string($db->link, $tw);
+                $ln = mysqli_real_escape_string($db->link, $ln);
+                $gp = mysqli_real_escape_string($db->link, $gp);
+                if ($fb == "" || $tw == "" || $ln == "" || $gp == "") {
+                    echo "<span class='error'>Field must not be empty !</span>";
+                } else {
+                    $query = "UPDATE dbl_social
+                        SET
+                        fb = '$fb',
+                        tw = '$tw',
+                        ln = '$ln',
+                        gp = '$gp'
+                        WHERE id = '1'";
+
+                     $updated_row = $db->insert($query);
+                    if ($updated_row) {
+                        echo "<span class='success'>Data Updated successfully!</span>";
+                    } else {
+                        echo "<span class='error'>Data Not Updated !</span>";
+                    }
+                }
+
+            }
+        ?>
+
         <div class="block">
-         <form>
+        <?php
+            $query = "SELECT * FROM dbl_social WHERE id = '1'";
+            $socialmedia = $db->select($query);
+            if ($socialmedia) {
+            while ($result = $socialmedia->fetch_assoc()) {
+        ?>
+         <form action="social.php" method="post">
             <table class="form">
                 <tr>
                     <td>
                         <label>Facebook</label>
                     </td>
                     <td>
-                        <input type="text" name="facebook" placeholder="Facebook link.." class="medium" />
+                        <input type="text" name="fb" value="<?php echo $result['fb']?>" class="medium" />
                     </td>
                 </tr>
                  <tr>
@@ -19,7 +58,7 @@
                         <label>Twitter</label>
                     </td>
                     <td>
-                        <input type="text" name="twitter" placeholder="Twitter link.." class="medium" />
+                        <input type="text" name="tw" value="<?php echo $result['tw']?>" class="medium" />
                     </td>
                 </tr>
 
@@ -28,7 +67,7 @@
                         <label>LinkedIn</label>
                     </td>
                     <td>
-                        <input type="text" name="linkedin" placeholder="LinkedIn link.." class="medium" />
+                        <input type="text" name="ln" value="<?php echo $result['ln']?>" class="medium" />
                     </td>
                 </tr>
 
@@ -37,7 +76,7 @@
                         <label>Google Plus</label>
                     </td>
                     <td>
-                        <input type="text" name="googleplus" placeholder="Google Plus link.." class="medium" />
+                        <input type="text" name="gp" value="<?php echo $result['gp']?>" class="medium" />
                     </td>
                 </tr>
 
@@ -49,6 +88,7 @@
                 </tr>
             </table>
             </form>
+            <?php } } ?>
         </div>
     </div>
 </div>
