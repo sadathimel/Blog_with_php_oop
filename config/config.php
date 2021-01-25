1ac41a0d2941ab9cf7c8ce79c12a2324c1fecf64
sadat <sadat.himel@gmail.com>
2021-01-25 16:01:38 +0600

    meta tag update dynamicly

--- config/config.php
+++ config/config.php
@@ -4,4 +4,5 @@ define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "db_blog");
 define("TITLE", "Learn with Sadat Himel");
+define("KEYWORDS", "PHP Tutorials, JAVA Tutorials, Oracle Database, C# Tutorials");
 
