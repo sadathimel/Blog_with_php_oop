4f7ee56566c17fbd0ed56986503221b889e47873
sadat <sadat.himel@gmail.com>
2021-01-25 14:00:06 +0600

    seen message and delete message

--- admin/inbox.php
+++ admin/inbox.php
@@ -1,8 +1,28 @@
 ﻿<?php include 'inc/header.php'; ?>
 <?php include 'inc/sidebar.php'; ?>
+
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Inbox</h2>
+
+        <?php 
+            if (isset($_GET['seenid'])) {
+                $seenid = $_GET['seenid'];
+
+                $query = "UPDATE tbl_contact
+                            SET
+                            status = '1'
+                            WHERE id = '$seenid'";
+
+                    $updated_row = $db->update($query);
+                    if ($updated_row){
+                        echo "<span class='success'>Message sent in the seen Box</span>";
+                    }else{
+                        echo "<span class='error'>Something Wrong</span>";
+                    }
+            }
+        ?>
+
         <div class="block">
             <table class="data display datatable" id="example">
             <thead>
@@ -33,7 +53,7 @@
                     <td>
                         <a href="viewmsg.php?msgid=<?php echo $result['id']; ?>">View</a> ||
                         <a href="replaymsg.php?msgid=<?php echo $result['id']; ?>">Replay</a>||
-                        <a href="?seenid=<?php echo $result['id']; ?>">Seen</a>
+                        <a onclick="return confirm('Are you sure to Move this message?');" href="?seenid=<?php echo $result['id']; ?>">Seen</a>
                     </td>
                 </tr>
             <?php } } ?>
@@ -44,9 +64,23 @@
 
     <div class="box round first grid">
         <h2>Seen Message</h2>
+
+        <?php
+            if (isset($_GET['delid'])) {
+                $delid = $_GET['delid'];
+                $delquery = "DELETE FROM tbl_contact WHERE id='$delid'";
+                $deldata = $db->delete($delquery);
+                if ($deldata) {
+                    echo "<span class='success'>Message Deleted Successfully.</span>";
+                }else{
+                    echo "<span class='error'>Message not Deleted.</span>";
+                }
+            }
+        ?>
+
         <div class="block">
             <table class="data display datatable" id="example">
-                <thead>
+            <thead>
                 <tr>
                     <th>Serial No.</th>
                     <th>Name</th>
@@ -55,21 +89,30 @@
                     <th>Date</th>
                     <th>Action</th>
                 </tr>
-                </thead>
-                <tbody>
+            </thead>
+            <tbody>
+            <?php
+                $query = "SELECT * FROM tbl_contact WHERE status = '1' ORDER BY id desc";
+                $msg = $db->select($query);
+                if ($msg) {
+                    $i = 0;
+                     while ($result = $msg->fetch_assoc()){
+                        $i++;
+            ?>
                 <tr class="odd gradeX">
-                    <td>01</td>
-                    <td>Name</td>
-                    <td>Email</td>
-                    <td>Message</td>
-                    <td>Date</td>
+                    <td><?php echo $i; ?></td>
+                    <td><?php echo $result['firstname'].' '.$result['lastname'];?></td>
+                    <td><?php echo $result['email'];?></td>
+                    <td><?php echo $fm->textShorten($result['body'], 30); ?></td>
+                    <td><?php echo $fm->formatDate($result['date']); ?></td>
                     <td>
-                        <a href="">Delete</a>
+                        <a href="viewmsg.php?msgid=<?php echo $result['id']; ?>">View</a> ||
+                        <a onclick="return confirm('Are you sure to Move this message?');" href="?delid=<?php echo $result['id']; ?>">Delete</a>
                     </td>
                 </tr>
-                </tbody>
-
-            </table>
+            <?php } } ?>
+            </tbody>
+        </table>
         </div>
     </div>
 </div>
