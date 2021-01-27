efc200bcd14c6c52a6ba7f4c354133f5a33fc0bc
sadat <sadat.himel@gmail.com>
2021-01-27 23:46:20 +0600

    separate css file from header file

---
+++ scripts/css.php
@@ -0,0 +1,166 @@
+<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="style.css">
+
+<style>
+
+/*default*/
+.headersection,.footersection {
+  background: #CA932F;
+}
+.icon a {
+	background: #a56e0a none repeat scroll 0 0;
+}
+.searchbtn input[type="text"] {
+	background: #ffe07c;
+	border: 1px solid #A56E0A;
+}
+.searchbtn input[type="submit"] {
+	border: 1px solid #9d6602;
+	
+}
+.searchbtn input[type="submit"]:hover {
+	border: 1px solid #e6af4b;
+	background: #b7801c;
+	
+}
+input[type="submit"] {
+	background: #b7801c none repeat scroll 0 0;
+	border: 1px solid #9d6602;
+}
+
+.navsection {
+	background: none repeat scroll 0 0 #e6af4b;
+}
+
+.navsection ul li {
+	border-left: 1px solid #fdc662;
+	border-right: 1px solid #c18a26;
+}
+.samepost h2 a {
+	color: #ac7511;
+}
+.maincontent,.sidebar {
+	background: #fef4e5 none repeat scroll 0 0;
+}
+.samepost img {
+	border: 1px solid #ebb450;
+}
+.contentsection {
+  border: 1px solid #ca932f;
+}
+.samesidebar h2 {
+	background: #e6af4b none repeat scroll 0 0;
+	border-bottom: 2px solid #b7801c;
+	color: #333;
+}
+.readmore a {
+	border: 1px solid #b7801c;
+	color: #b7801c;
+}
+.pagination a {
+	background: #E6AF4B none repeat scroll 0 0;
+	border: 1px solid #a7700c;
+	color: #333;
+}
+.pagination a:hover {
+  background: #be8723 none repat scroll 0 0;
+}
+.footermenu ul li a {
+	color: #E6AF4B;
+}
+.samepost h2, .about h2 {
+	color: #ac7511;
+}
+.about img {
+	border: 1px solid #b7801c;
+}
+.relatedpost h2 {
+	background: #e6af4b none repeat scroll 0 0;
+	border-bottom: 2px solid #b7801c !important;
+}
+
+
+
+
+
+/*green*/
+.headersection,.footersection {
+  background: #008000;
+}
+.icon a {
+	background: #49C949 none repeat scroll 0 0;
+}
+.searchbtn input[type="text"] {
+	background: #52c252;
+	border: 1px solid #034703;
+}
+.searchbtn input[type="submit"] {
+	border: 1px solid #549854;
+	
+}
+.searchbtn input[type="submit"]:hover {
+	border: 1px solid #52c252;
+	background: #005600;
+	
+}
+input[type="submit"] {
+	background: #005600;
+	border: 1px solid #007000;
+}
+.navsection {
+	background: none repeat scroll 0 0 #35a535;
+}
+.navsection ul li {
+	border-left: 1px solid #007000;
+	border-right: 1px solid #38a838;
+}
+.samepost h2 a {
+	color: #007000;
+}
+.maincontent,.sidebar {
+	background: #c9ffc9 none repeat scroll 0 0;
+}
+.samepost img {
+	border: 1px solid #35a535;
+}
+.contentsection {
+  border: 1px solid #35a535;
+}
+.samesidebar h2 {
+	background: #35a535 none repeat scroll 0 0;
+	border-bottom: 2px solid #007000;
+	color: #fff;
+}
+.readmore a {
+	border: 1px solid #35a535;
+	color: #35a535;
+}
+.readmore a:hover {
+	color: #007000;
+}
+.pagination a {
+	background: #35a535 none repeat scroll 0 0;
+	border: 1px solid #007000;
+	color: #fff;
+}
+.pagination a: {
+	background: #007000 none repeat scroll 0 0;
+	border: 1px solid #35a535;
+	color: #fff;
+}
+.footermenu ul li a {
+	color: #fff;
+}
+.samepost h2, .about h2 {
+	color: #007000;
+}
+.about img {
+	border: 1px solid #007000;
+}
+.relatedpost h2 {
+	background: #35a535 none repeat scroll 0 0;
+	border-bottom: 2px solid #007000 !important;
+}
+/*red*/
+</style>
